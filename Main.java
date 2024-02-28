0775b632ab735e5f1f7db7f2bb5220c900d40b81
mohammedfaizaansharief1 <mohdfaizaansharief@gmail.com>
2024-02-28 08:27:42 +0530

    initial Program

---
+++ Main.java
@@ -0,0 +1,39 @@
+package Java;
+import java.util.*;
+
+public class Main{
+    // int x = 10;
+    public static void main(String args[]){
+        // System.out.print("Hello World");
+        // Main Obj = new Main();
+        // System.out.println(Obj.x);
+        
+
+        // Scanner s = new Scanner(System.in);
+        // System.out.println("Enter the name, age and salary:");
+        // String name= s.nextLine();
+        // int age = s.nextInt();
+        // double salary = s.nextDouble();
+        // System.out.println("The enter details are: " + name + age + salary);
+
+        // Arithematic operators --> + -  *  /  %
+        int a;
+        int b,c;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the values of a:");
+        a = s.nextInt();
+        System.out.println("Enter the values of b:");
+        b = s.nextInt();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        // Logical operators --> && || !
+
+
+        // Bitwise operators --> & | ^ >> << ~
+        
+    }
+}
